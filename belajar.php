de55c71b0d98cf9b242f2c3c9bb728a453d054c0
syarif-muhammad-iqbal <syarifiqbal5@gmail.com>
2021-02-24 15:32:37 +0700

    belajar php

---
+++ belajar.php
@@ -0,0 +1,12 @@
+<?php
+// cetak string
+$nama = 'Bale';
+$umur = 18;
+$berat = 35.5;
+
+echo 'Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur.' Tahun';
+echo '<br/>Berat : ' . $berat.' Kg';
+
+echo "<br/>Hallo Gaes!! $nama Apakabar";
+?>
---
+++ coba.php
@@ -0,0 +1,32 @@
+<?php
+ 
+
+
+
+    $angka = 1;
+    $nama = "bale";
+    $nama_lengkap = 'syarif muhammad iqbal';
+    $angka_desimal = 2.25;
+
+
+
+    echo $angka;
+    echo "<br/>";
+    echo $nama;
+    echo "<br/>";
+    echo $nama_lengkap;
+    echo "<br/";
+    echo $angka_desimal;
+
+    echo "<br/>";
+    echo "dokumen saya : ".$_SERVER["DOCUMENT_ROOT"];
+
+    echo "<br/>";
+    echo "nama saya ".$nama_lengkap;
+
+
+
+
+
+
+?>
